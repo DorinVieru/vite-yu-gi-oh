1d8f69173d9efdacc46ff0492e13db87bc062228
DorinVieru <dorino.vieru@gmail.com>
2024-01-10 13:45:15 +0100

    [UP] Create, clean & modify all necessary files to work, create styles folder for sass & create generals settings for css

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<!-- SEZIONE JS SCRIPT -->
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<!-- SEZIONE HTML -->
+<template lang="">
+  <header>
+    
+  </header>
+</template>
+
+<!-- SEZIONE STYLE -->
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<!-- SEZIONE JS SCRIPT -->
+<script>
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<!-- SEZIONE HTML -->
+<template lang="">
+<div>
+
+</div>  
+</template>
+
+<!-- SEZIONE STYLE -->
+<style lang="scss" scoped>
+
+</style>
